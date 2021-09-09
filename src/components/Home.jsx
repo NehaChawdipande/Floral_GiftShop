177f3817e9f104f277ef4aec883cdf61aaab059c
NehaChawdipande <nehachawdipande10@gmail.com>
2021-09-09 13:14:47 +0530

    commit on main

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,6 +11,7 @@ export default function Home(){
                 <hr/>
                 <p>Propelled by our affection for flowers and energy about nature, it is our obsession to make creative and noteworthy plant and new flower courses of action. We trust that the best, freshest flowers deliver the most noteworthy presentations.
 Floral Affairs is glad to give quality flowers and plants to our customers. Our retail premises opened in 2012 after much interest from the London open who respected our orchids at the Chelsea Flower Show. From that point forward, we have bloomed into a full administration flower specialist with both private and corporate customers. We owe quite a bit of our prosperity to our extraordinary customers who move us to make unique and dazzling structures. Our master staff is here to enable all customers to pick the best flowers and plants for all events and purposes</p>
+                <hr/>
                 <Link to="/ProductList"><button className="btn btn-dark">Get started!</button></Link>
 
             </div>
